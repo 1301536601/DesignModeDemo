4b23caad59cdd30e9350ec5c3660adcf210aa297
benxiong.hu <1301536601@qq.com>
2021-04-08 21:26:37 +0800

    新增工厂模式

---
+++ DesignMode/FactoryMode/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace FactoryMode
+{
+    /// <summary>
+    /// 工厂模式
+    /// 和简单工厂的区别：
+    /// 简单工厂，在类的内部进行修改
+    /// 工厂模式：在类的内部进行修改
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory factory = new StudentFactory();
+            var leiFeng = factory.GetFactory();
+            leiFeng.BuyRice();
+            Console.ReadKey();
+        }
+    }
+
+    public interface IFactory
+    {
+        LeiFeng GetFactory();
+    }
+
+    public class StudentFactory : IFactory
+    {
+        public LeiFeng GetFactory()
+        {
+            return new StudentLeiFeng();
+        }
+    }
+
+
+    public class VolunteerFactory : IFactory
+    {
+        public LeiFeng GetFactory()
+        {
+            return new VolunteerLeiFeng();
+        }
+    }
+
+
+
+    public interface LeiFeng
+    {
+
+        /// <summary>
+        /// 买米
+        /// </summary>
+        void BuyRice();
+
+        /// <summary>
+        /// 洗衣服
+        /// </summary>
+        void WashCloths();
+
+        /// <summary>
+        /// 清理
+        /// </summary>
+        void CleanUp();
+    }
+
+    public class StudentLeiFeng : LeiFeng
+    {
+        public void BuyRice()
+        {
+            Console.WriteLine("学生正在买米");
+        }
+
+        public void WashCloths()
+        {
+            Console.WriteLine("学生正在买米");
+        }
+
+        public void CleanUp()
+        {
+            Console.WriteLine("学生正在清理");
+        }
+    }
+
+    public class VolunteerLeiFeng : LeiFeng
+    {
+        public void BuyRice()
+        {
+            Console.WriteLine("学生正在买米");
+        }
+
+        public void WashCloths()
+        {
+            Console.WriteLine("学生正在买米");
+        }
+
+        public void CleanUp()
+        {
+            Console.WriteLine("学生正在清理");
+        }
+    }
+
+}
