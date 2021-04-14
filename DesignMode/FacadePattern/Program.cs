2ad4ea55d3056b6b75ce7f83bbd56627598dc0f9
benxiong.hu <1301536601@qq.com>
2021-04-14 21:38:22 +0800

    新增外观模式

---
+++ DesignMode/FacadePattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FacadePattern
+{
+
+    /// <summary>
+    /// 外观模式
+    /// 使用了依赖倒置原则和迪米特原则  将下层的方法和上层隔开
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new CollectA().Get();
+            new CollectB().Get();
+        }
+
+        public class CollectA
+        {
+            public void Get()
+            {
+                new MethodA().Get();
+                new MethodB().Get();
+            }
+        }
+
+        public class CollectB
+        {
+            public void Get()
+            {
+                new MethodA().Get();
+                new MethodC().Get();
+            }
+        }
+
+        public class MethodA
+        {
+            public void Get()
+            {
+                Console.WriteLine($@"{GetType()}");
+            }
+        }
+
+        public class MethodB
+        {
+            public void Get()
+            {
+                Console.WriteLine($@"{GetType()}");
+            }
+        }
+
+        public class MethodC
+        {
+            public void Get()
+            {
+                Console.WriteLine($@"{GetType()}");
+            }
+        }
+    }
+}
