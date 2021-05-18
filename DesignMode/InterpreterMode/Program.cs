48f28205edf5cbf4f9ab02f710e00b7bbe8e315f
benxiong.hu <33918700+1301536601@users.noreply.github.com>
2021-05-18 22:10:55 +0800

    新增访问器/解释器设计模式

---
+++ DesignMode/InterpreterMode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterpreterMode
+{
+
+    /// <summary>
+    /// 解释器模式
+    /// 如果一种特定类型问题发生的频率足够高，那么就可以将这些事例表述为一个简单语言的橘子，这样就可以构建一个解释器
+    /// 问题：不理解
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+            var expressions = new List<Expression>
+            {
+                new TerminalExpression(),
+                new TerminalExpression(),
+                new TerminalExpression(),
+                new NotTerminalExpression()
+            };
+            foreach (var item in expressions)
+            {
+                item.Interpreter(context);
+            }
+
+            Console.ReadKey();
+        }
+
+        public abstract class Expression
+        {
+            public abstract void Interpreter(Context context);
+        }
+
+        public class TerminalExpression : Expression
+        {
+            public override void Interpreter(Context context)
+            {
+                Console.WriteLine("终端解释器");
+            }
+        }
+
+        public class NotTerminalExpression : Expression
+        {
+            public override void Interpreter(Context context)
+            {
+                Console.WriteLine("非终端解释器");
+            }
+        }
+
+        public class Context
+        {
+            /// <summary>
+            /// 输入
+            /// </summary>
+            public string Input { get; set; }
+
+            /// <summary>
+            /// 输出
+            /// </summary>
+            public string OutPut { get; set; }
+        }
+
+
+    }
+}
---
+++ DesignMode/VisitorMode/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisitorMode
+{
+    /// <summary>
+    /// 访问者模式
+    /// 表示一个作用于某对象杰克狗中的各元素的操作，在不改变个元素的前提下作用于这些元素的新操作
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new ObjectStructure();
+            o.Add(new ConcreteElementA());
+            o.Add(new ConcreteElementB());
+            o.Add(new ConcreteElementB());
+            o.Delete(new ConcreteElementB());
+            var v1 = new ConcreteVisitor1();
+            var v2 = new ConcreteVisitor2();
+            o.Accept(v1);
+            o.Accept(v2);
+            Console.ReadKey();
+        }
+
+        public class ObjectStructure
+        {
+            private List<Element> _elements = new List<Element>();
+
+            public void Add(Element element)
+            {
+                _elements.Add(element);
+            }
+
+            public void Delete(Element element)
+            {
+                _elements.Remove(element);
+            }
+
+            public void Accept(Visitor visitor)
+            {
+                foreach (var item in _elements)
+                {
+                    item.Accept(visitor);
+                }
+            }
+        }
+
+        public abstract class Visitor
+        {
+            public abstract void VisitConcreteElementA(ConcreteElementA concreteElementA);
+
+            public abstract void VisitConcreteElementB(ConcreteElementB concreteElementB);
+        }
+
+        public class ConcreteVisitor1 : Visitor
+        {
+            public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+            {
+                Console.WriteLine($@"{concreteElementA.GetType().Name}被{this.GetType().Name}访问");
+            }
+
+            public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+            {
+                Console.WriteLine($@"{concreteElementB.GetType().Name}被{this.GetType().Name}访问");
+            }
+        }
+
+        public class ConcreteVisitor2 : Visitor
+        {
+            public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+            {
+                Console.WriteLine($@"{concreteElementA.GetType().Name}被{this.GetType().Name}访问");
+            }
+
+            public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+            {
+                Console.WriteLine($@"{concreteElementB.GetType().Name}被{this.GetType().Name}访问");
+            }
+        }
+
+        public abstract class Element
+        {
+            public abstract void Accept(Visitor visitor);
+        }
+
+        public class ConcreteElementA : Element
+        {
+            public override void Accept(Visitor visitor)
+            {
+                visitor.VisitConcreteElementA(this);
+            }
+        }
+
+
+        public class ConcreteElementB : Element
+        {
+            public override void Accept(Visitor visitor)
+            {
+                visitor.VisitConcreteElementB(this);
+            }
+        }
+    }
+}
